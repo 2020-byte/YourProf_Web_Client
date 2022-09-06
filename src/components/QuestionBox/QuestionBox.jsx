a2bcdd9079f2fad1d9787283722b1d5e65930375
2020-byte <gmmoon0824@gmail.com>
2022-09-06 12:58:03 +0900

    rate page demo

--- src/components/QuestionBox/QuestionBox.jsx
+++ src/components/QuestionBox/QuestionBox.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import courses from '../../data/profCourseItems.json';
 import grades from '../../data/grades.json';
 import SelectBox from '../SelectBox.jsx/SelectBox';
@@ -25,10 +26,19 @@ const QuestionBox = ({question}) => {
         setSelected(selected);
     }
 
+    const allSet = true;
+
+    const unbuttonStyle = {
+        backgroundColor: '#EDEDED',
+        boxShadow: 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;',
+        border: 'grey',
+        color: '#A4A3A3'
+    }
+
 
     return (
         <div className={styles.body}>
-            <div className={styles.question}>{question.question}</div>
+            <div className={styles.question}>{question.value !== 'tos' && question.question}</div>
                 <div className={styles.answerBox}>
                 {
                     question.type === 'select' &&
@@ -58,6 +68,15 @@ const QuestionBox = ({question}) => {
                 {
                     question.type === 'textarea' &&
                     <ReviewBox name={question.value}/>
+                }
+                {
+                    question.value === 'tos' &&
+                    <div className={styles.submitBox}>
+                        <p>
+                            {question.question}
+                        </p>
+                        <Button style={ !allSet? unbuttonStyle: {}} className={styles.button}>Submit Rating</Button>
+                    </div>
                 }
             </div>
         </div>
--- src/page/RateProf.jsx
+++ src/page/RateProf.jsx
@@ -3,6 +3,14 @@ import { Stack } from 'react-bootstrap';
 import QuestionBox from '../components/QuestionBox/QuestionBox';
 import questions from '../data/question.json';
 
+const TOS = {
+    'value': 'tos',
+    'type' : 'text',
+    'question':
+    `By clicking the "Submit" button, I acknowledge that I have read and agreed to the Rate My Professors Site Guidelines, 
+    Terms of Use and Privacy Policy. Submitted data becomes the property of RateMyProfessors.com. IP addresses are logged.`
+}
+
 const RateProf = (props) => {
 
 
@@ -13,6 +21,7 @@ const RateProf = (props) => {
                     <QuestionBox key={i.id} question={i} />
                 ))
             }
+            <QuestionBox question={TOS} />
         </Stack>
     )
 }
