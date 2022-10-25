b132a4b562a3c6c20b33b01b76b51c55a31c7d0b
2020-byte <gmmoon0824@gmail.com>
2022-10-24 18:09:33 -0700

    Apply regexp to the sign in function

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button, Form, Stack } from 'react-bootstrap';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContent';
@@ -11,6 +11,12 @@ const bodyStyle = {
     boxShadow: "rgb(50 50 93 / 25%) 0px 2px 5px -1px, rgb(0 0 0 / 30%) 0px 1px 3px -1px",
 }
 
+const warning = {
+    margin: "0% 2% 0% 2%",
+    fontSize: "90%",
+    color: "rgb(220, 63, 63)"
+}
+
 
 const Login = ({onSignUp, onLogin}) => {
     const {user} = useAuth();
@@ -19,13 +25,31 @@ const Login = ({onSignUp, onLogin}) => {
         user && navigate("/");
     }, [user]);
 
+    const passwordRef = useRef();
     const [signup, setSignup] = useState(false);
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [checkPassword, setCheckPassword] = useState('');
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     // const [text, setText] = useState('');
     // const [isAlert, setIsAlert] = useState(false);
+
+    const [usernameError, setUsernameError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [passwordCheckError, setPasswordCheckError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [nameError, setNameError] = useState(false);
+
+
+    const usernameForm = /^[a-zA-z0-9]{4,}$/;;
+    const passwordForm = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+    const emailForm = /@student.ubc.ca$/;
+    
+    //TODO: `이거 체크안해주는데?? 이거 알아보고 고치기
+    const nameForm = /^[a-zA-z0-9]{4,12}$/;
+
+
 
     const onSubmit = (event) => {
         event.preventDefault();
@@ -47,13 +71,25 @@ const Login = ({onSignUp, onLogin}) => {
         } = event;
         switch (name) {
         case 'username':
-            return setUsername(value);
+            setUsername(value);
+            setUsernameError(usernameForm.test(value));
+            return 
         case 'password':
-            return setPassword(value);
+            setPassword(value);
+            setPasswordError(passwordForm.test(value));
+            return 
+        case 'checkPassword':
+            setCheckPassword(value);
+            setPasswordCheckError(passwordRef.current.value === value)
+            return 
         case 'name':
-            return setName(value);
+            setName(value);
+            setNameError(nameForm.test(value));
+            return 
         case 'email':
-            return setEmail(value);
+            setEmail(value);
+            setEmailError(emailForm.test(value));
+            return 
         case 'signup':
             return setSignup(checked);
         default:
@@ -73,7 +109,9 @@ const Login = ({onSignUp, onLogin}) => {
                     className='form-input'
                     required
                     />
+                    {signup && !usernameError  && username && <p style={warning}>Please enter at least 4 digits using a combination of only uppercase and lowercase letters or numbers</p>}
                     <Form.Control
+                    ref={passwordRef}
                     name='password'
                     type='password'
                     placeholder='Password'
@@ -81,6 +119,18 @@ const Login = ({onSignUp, onLogin}) => {
                     className='form-input'
                     onChange={onChange}
                     />
+                    {signup && !passwordError  && password && <p style={warning}>Please enter at least 8 digits using a combination of numbers + English letters + special characters!</p>}
+                    {signup && (
+                    <Form.Control
+                    name='checkPassword'
+                    type='password'
+                    placeholder='Password Check'
+                    value={checkPassword}
+                    className='form-input'
+                    onChange={onChange}
+                    />
+                    )}
+                    {signup && !passwordCheckError && checkPassword && <p style={warning}>Passwords do not match!</p>}
                     {signup && (
                     <Form.Control
                         name='name'
@@ -92,6 +142,7 @@ const Login = ({onSignUp, onLogin}) => {
                         required
                     />
                     )}
+                    {signup && !nameError && name && <p style={warning}>Please enter 4 to 12 digits using a combination of only uppercase and lowercase letters or numbers</p>}
                     {signup && (
                     <Form.Control
                         name='email'
@@ -103,6 +154,7 @@ const Login = ({onSignUp, onLogin}) => {
                         required
                     />
                     )}
+                    {signup && !emailError && (<p style={warning}>Email must end with @student.ubc.ca</p>)}
                 </Stack>
                 <div className='form-signup'>
                 <input
